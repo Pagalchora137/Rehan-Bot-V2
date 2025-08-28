f66df583a5352a91a973c68eb331cff329670b2f
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 00:24:49 +0530

    Update ai.js

--- Priyansh/commands/ai.js
+++ Priyansh/commands/ai.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "ai",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐑𝐞𝐡𝐚ñ 𝐒𝐡𝐚𝐢𝐤𝐡",
     description: "BlackBoxAi by Priyansh",
     commandCategory: "ai",
     usages: "[ask]",
