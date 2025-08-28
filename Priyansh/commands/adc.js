3308037b9b21c31398594fa7a15c2794d202df8b
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 00:16:10 +0530

    Update adc.js

--- Priyansh/commands/adc.js
+++ Priyansh/commands/adc.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "adc",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
     description: "Apply code from buildtooldev and pastebin",
     commandCategory: "Admin",
     usages: "[reply or text]",
