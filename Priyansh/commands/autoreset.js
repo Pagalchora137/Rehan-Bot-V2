4ebb685e16693a5af4aa921090841c5da7e1eed7
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 08:08:49 +0530

    Update autoreset.js

--- Priyansh/commands/autoreset.js
+++ Priyansh/commands/autoreset.js
@@ -2,7 +2,7 @@ module. exports. config = {
     name: "autoreset",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
     description: "AUTO RESTART",
     commandCategory: "System",
     cooldowns: 5
