57e22632bb1decbdcb912ded29d99a7c5b7901a5
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 11:26:00 +0530

    Update cardinfov4.js

--- Priyansh/commands/cardinfov4.js
+++ Priyansh/commands/cardinfov4.js
@@ -7,7 +7,7 @@ module.exports.config = {
   name: "cardinfov4",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
   description: "Tạo card thông tin người dùng facebook",
   commandCategory: "info",
   usages: "",
