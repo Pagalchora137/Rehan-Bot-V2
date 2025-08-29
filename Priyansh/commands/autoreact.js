379e10759e70cc263a621d97b8e52bd762a0f1e8
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 08:05:01 +0530

    Update autoreact.js

--- Priyansh/commands/autoreact.js
+++ Priyansh/commands/autoreact.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "autoreact",
   version: "1.1.1",
   hasPermission: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
   description: "Bot React",
   commandCategory: "No Prefix",
   usages: '[]',
