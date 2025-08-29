9e4cd6fc0c08d80e80ab842ece5a6a3df746ce48
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 11:30:50 +0530

    Update checktt.js

--- Priyansh/commands/checktt.js
+++ Priyansh/commands/checktt.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "checktt",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
 	description: "interactive check",
 	commandCategory: "Utilities",
 	usages: "checktt",
