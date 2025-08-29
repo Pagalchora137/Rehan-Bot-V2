f726ae9ac48ededee1d447fc03dfdccc1085a179
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 11:57:57 +0530

    Update darock.js

--- Priyansh/commands/darock.js
+++ Priyansh/commands/darock.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "vineboom",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡", 
 	description: "hihihihi",
 	commandCategory: "Không cần dấu lệnh",
 	usages: "vineboom",
