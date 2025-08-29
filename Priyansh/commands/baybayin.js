5d2b81e5cc907dd3d2efa3afaecb939efdb250e7
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 10:34:59 +0530

    Update baybayin.js

--- Priyansh/commands/baybayin.js
+++ Priyansh/commands/baybayin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "baybayin",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
 	description: "convert text to baybayin",
 	commandCategory: "Phương tiện",
 	usages: "baybayin [text]",
