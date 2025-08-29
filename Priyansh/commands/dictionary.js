f2b34de246f7a008b38f266c15e35f4eaf001018
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 12:28:37 +0530

    Update dictionary.js

--- Priyansh/commands/dictionary.js
+++ Priyansh/commands/dictionary.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "dictionary",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
 	description: "Check dictionary",
   	usage: "[text]",
 	commandCategory: "Utilities",
