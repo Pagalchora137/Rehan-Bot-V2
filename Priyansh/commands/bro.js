a2cc3fc031c49bfde2a191125796d2a17c96915a
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 11:16:58 +0530

    Update bro.js

--- Priyansh/commands/bro.js
+++ Priyansh/commands/bro.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bro",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: " Priyansh Rajput", 
+  credits: " 𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
