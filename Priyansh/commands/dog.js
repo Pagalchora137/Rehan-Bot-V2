ea1ad65aadda1fa69324353b0346edda07762326
Pagalchora137 <pagalchora202@gmail.com>
2025-08-29 12:29:17 +0530

    Update dog.js

--- Priyansh/commands/dog.js
+++ Priyansh/commands/dog.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "dog",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝐑𝐞𝐡𝐚𝐧 𝐒𝐡𝐚𝐢𝐤𝐡",
 	description: "Xem Boss",
 	commandCategory: "Picture",
 	usages: "dog [Text]",
